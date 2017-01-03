b61e81360aaa895f421efafb0983198fad552364
Greg <gmengler@gmail.com>
2017-01-03 15:31:38 -0500

    add exercise 5

---
+++ CreatingVariables.java
@@ -0,0 +1,36 @@
+public class CreatingVariables {
+	public static void main( String[] args ) {
+		int x, y, age, height;
+		double seconds, e, checking, travel;
+		String firstName, lastName, title, make, model;
+
+		x = 10;
+		y = 400;
+		age = 39;
+		height = 6;
+
+		seconds = 4.71;
+		e = 2.71828182845904523536;
+		checking = 1.89;
+		travel = 5;
+
+		firstName = "Graham";
+		lastName = "Mitchell";
+		title = "Mr.";
+		make = "Specialized";
+		model = "Stumpjumper";
+
+
+
+		System.out.println( "The variable x contains " + x );
+		System.out.println( "The value" + y + " is stored in the variable y." );
+		System.out.println( "The Experiment took " + seconds + " seconds." );
+		System.out.println( "A favorite irrational # is Euler's number: " + e );
+		System.out.println( "Hopefully you have more than $" + checking + "!" );
+		System.out.println( "My name's " + title + " " + firstName + lastName );
+		System.out.println( "My bike, the" + " " + make + " " + model + ", has" + " " + travel + " inches of travel" );
+		System.out.println( "It's comfortable since I am over" + " " + height + " feet tall." );
+
+
+	}
+}
