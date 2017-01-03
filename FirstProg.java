1ddd231ac8cbabe065e07b2b1dd80be936a1c186
Greg <gmengler@gmail.com>
2017-01-02 21:45:32 -0500

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main( String[] args ) {
+		System.out.println( "I, Greg Engler, am determined to learn how to code." );
+		System.out.println( "Today's date is January 2nd, 2017" );
+	}
+}
